8cea1a9ee72f8cc8c56ab85b92656332d0c4bf36
embrana <67813424+embrana@users.noreply.github.com>
2024-11-16 02:28:55 +0000

    test

--- src/app.py
+++ src/app.py
@@ -27,16 +27,41 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
         "hello": "world",
         "family": members
     }
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member_by_id(id):
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.get_member(id)
+    if member != None:
+        return jsonify(member), 200
+    return jsonify({"msg":"Not found"}), 404
+
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    request_body = request.get_json()
+    new_member= jackson_family.add_member(request_body)
+    return jsonify(new_member), 200
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member_by_id(id):
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.delete_member(id)
+    if member != None:
+        return jsonify({"done":True}), 200
+    return jsonify({"msg":"Not found"}), 404
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
